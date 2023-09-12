85c7d4299ec7e4de0a694f647679668fcc1e3b7f
lachlanbegbie <112054325+lachlanbegbie@users.noreply.github.com>
2023-09-12 11:58:52 +1000

    12_Sep_11_57
    - Gets date
    - Displays date in OTD
    - Displays 5 items with correlation to the current date

--- assets/js/onThisDay.js
+++ assets/js/onThisDay.js
@@ -1,31 +1,59 @@
 
 const apikey = "000ASNCw2E80ybfHsPYbb6yQ2Lp1ABJB";
 
-const nmaDate = "https://data.nma.gov.au/object?limit=5&temporal=-09&format=simple";
+// const nmaDate = "https://data.nma.gov.au/object?limit=5&temporal=" + dateCode + "&format=simple";
 
-
-async function getExhibitions() {
+async function getDateItem() {
     const onThisDay = document.getElementById('onThisDay');
+    const todayDate = document.getElementById('dateToday');
+
+
 
     try {
+        // Get today's date split into day, month and year
+        const d = new Date();
+        const day = d.getDate();
+        const month = d.getMonth();
+        const year = d.getFullYear();
+
+        // Find the string name of the current month
+        function getMonthName(month) {return d.toLocaleString([], { month: 'long' });}
+        // console.log(getMonthName(month));
+
+        // Compile date into string and place in DOM
+        const dateItem = document.createElement('div');
+        const dateString = `<h2>${day} ${getMonthName(month)} ${year}</h2>`;
+        dateItem.innerHTML = dateString;
+        // console.log(dateString);
+        todayDate.appendChild(dateItem);
+
+        // Create a 'date code' to input back into the NMA API call
+        dateCode = day + " " + getMonthName(month);
+        // dateCode = `${(month + 1)}-${day}`;
+        // console.log(dateCode);
+        
+
+        const nmaDate = "https://data.nma.gov.au/object?limit=5&temporal=" + dateCode + "&format=simple";
+
+
+        // Get data and add records from NMA
         const response = await fetch(nmaDate);
         const data = await response.json();
 
-        console.log(data);
-
+        // console.log(data);
 
         data.data.forEach(item => {
-            console.log(item);
+            // console.log(item);
 
-            const title = 'Item ' + item.id + ': "' + item.title + '"';
+            const title = "Item " + item.id + ": " + item.title;
             const description = item.physicalDescription;
-            console.log(title);
+            // console.log(title);
 
             const containerItem = document.createElement('div');
             containerItem.className = "dayRecord";
 
-            const divTitle = '<h3>' + title + '</h3>';
-            const divDes = '<p class="recordSummary">' + description + '</p>';
+            const divTitle = `<h3>${title}</h3>`;
+            const divDes = `<p class="recordSummary">${description}</p>`;
 
             containerItem.innerHTML = divTitle + divDes;
 
@@ -38,4 +66,4 @@ async function getExhibitions() {
 }
 
 // Call the function so it executes
-getExhibitions()
+getDateItem()
