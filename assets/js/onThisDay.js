d441c4f3d952dae777c25b2e5abfa262cfd96ea7
lachlanbegbie <112054325+lachlanbegbie@users.noreply.github.com>
2023-10-01 22:29:23 +1100

    1_Oct_22_28
    - Images code cleanup and other

--- assets/js/onThisDay.js
+++ assets/js/onThisDay.js
@@ -7,8 +7,6 @@ async function getDateItem() {
     const onThisDay = document.getElementById('onThisDay');
     const todayDate = document.getElementById('dateToday');
 
-
-
     try {
         // Get today's date split into day, month and year
         const d = new Date();
@@ -30,54 +28,58 @@ async function getDateItem() {
         // Create a 'date code' to input back into the NMA API call
         dateCode = day + " " + getMonthName(month);
         // dateCode = `${(month + 1)}-${day}`;
-         console.log(dateCode);
+        // console.log(dateCode);
 
 
-        const nmaDate = "https://data.nma.gov.au/object?limit=30&temporal=" + dateCode + "&format=simple";
-
+        const nmaDate = "https://data.nma.gov.au/object?limit=100&temporal=" + dateCode + "&format=simple";
 
         // Get data and add records from NMA
         const responseDate = await fetch(nmaDate);
         const dataDate = await responseDate.json();
 
-        // console.log(data);
+        console.log(dataDate);
 
         dataDate.data.forEach(item => {
 
-						//check to see if they 'hasversion'
-						if (item.hasVersion != null) {
+            // only returns if "hasVersion" (image) data is in the API
+            if (item.hasVersion != null) {
+                console.log(item);
 
-							// the image thing is very bizarre.
-							// I can see there are other images in the meta field,
-							// but they don't seem to be retrievable via this API
-							console.log(item.hasVersion[0].hasVersion[0]);
+                // returns the year of the item
+                const itemYear = item.temporal[0].startDate.slice(0,4);
 
-							//BEN: you should include the date for each item too, or at least the year so we can see how it relates
+                // builds the body of text
+                const title = "Item " + item.id + ": " + item.title;
+                const description = item.physicalDescription;
+                
+                // image of item
+                const img = item.hasVersion[0].hasVersion;
+                // const image = item.hasVersion[0].hasVersion[0].identifier;
+                
+                for (i=0; i < img.length; i++) {
+                    if (img[i].version === 'large image') {
+                        // console.log(img[i].identifier);
+                        image = img[i].identifier;
+                    }
+                }							
 
-							const title = "Item " + item.id + ": " + item.title;
-							const description = item.physicalDescription;
-							const image = item.hasVersion[0].hasVersion[0].identifier;
-							// const image = "assets/img/whitesquare.png";
-							// console.log(title);
-							console.log(image);
-
-							const containerItem = document.createElement('div');
-							containerItem.className = "dayRecord";
-
-
-							const divImg = `<img src="${image}" class="dayRecordImg">`
-							const divTitle = `<h3>${title}</h3>`;
-							const divDes = `<p class="recordSummary">${description}</p>`;
-
-							containerItem.innerHTML = `${divImg} <div class="dayRecordDes"> ${divTitle} ${divDes} </div>`;
-
-							onThisDay.appendChild(containerItem);
-						} //close if
+                const containerItem = document.createElement('div');
+                containerItem.className = "dayRecord";
 
 
+                const divImg = `<img src="${image}" class="dayRecordImg">`
+                const divTitle = `<h3>${itemYear} - ${title}</h3>`;
+                const divDes = `<p class="recordSummary">${description}</p>`;
+
+                containerItem.innerHTML = `${divImg} <div class="dayRecordDes">${divTitle} ${divDes} </div>`;
+
+                onThisDay.appendChild(containerItem);
+            }
+
+            
         });
 
-        // // See more button here
+        // // See more button here?
     } catch (error) {
         console.log('error: ', error);
     }
